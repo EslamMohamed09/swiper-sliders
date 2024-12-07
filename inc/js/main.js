14eadbce058a956682368835a3acd25691bc73ac
EslamMayo09 <eslammayo2022@gmail.com>
2024-12-07 23:57:58 +0200

    first two sections of swiper sliders

---
+++ inc/js/main.js
@@ -0,0 +1,46 @@
+var swiper1 = new Swiper(".swiper1", {
+  effect:"coverflow",
+  grabCursor:true,
+  centeredSlides:true,
+  initialSlide:5,
+  speed:600,
+  preventClicks:true,
+  slidesPerView:"auto",
+  coverflowEffect:{
+    rotate:0,
+    stretch:110,
+    depth:350,
+    modifier:1,
+    slideShadow:true,
+  },
+  on:{
+    click(event){
+        swiper1.slideTo(This.clickedIndex);
+    },
+  },
+});
+
+var swiper2 = new Swiper(".swiper2", {
+    effect:"coverflow",
+    grabCursor:true,
+    centeredSlides:true,
+    initialSlide:3,
+    speed:600,
+    preventClicks:true,
+    slidesPerView:"auto",
+    coverflowEffect:{
+      rotate:0,
+      stretch:170,
+      depth:450,
+      modifier:1,
+      slideShadow:true,
+    },
+    on:{
+      click(event){
+          swiper2.slideTo(this.clickedIndex);
+      },
+    },
+    pagination: {
+        el:".swiper-pagination",
+    },
+});
