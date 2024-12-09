531b022af6b1816f2dd01f8b17b216905ffb38a7
EslamMayo09 <eslammayo2022@gmail.com>
2024-12-10 01:09:57 +0200

    Finishing swiper 5 section

--- inc/js/main.js
+++ inc/js/main.js
@@ -136,7 +136,7 @@ function flipActiveSlide(slidesElement, activeSlideElement){
 flipActiveSlide(".swiper4-container .swiper-slide", ".swiper4-container .swiper-slide-active");
 
 
-  const swiper5TextSlider = new Swiper(".swiper-section5 .text-swiper5-container", {
+  const swiper5TextSlider = new Swiper(".text-swiper5-container", {
     effect: "slide",
     direction: "vertical",
     speed: 1000,
