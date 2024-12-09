286a38629f8252d0100eb370d9442b75db9c3a34
EslamMayo09 <eslammayo2022@gmail.com>
2024-12-09 12:17:54 +0200

    Adjustment in swiper 3 style (cube effect)

--- inc/js/main.js
+++ inc/js/main.js
@@ -52,27 +52,49 @@ var swiper2 = new Swiper(".swiper-section2 .swiper2-container", {
 
 document.addEventListener('DOMContentLoaded', () => {
   const swiper3 = new Swiper('.swiper3', {
-    effect: 'cube',
-    grabCursor: true,
-    cubeEffect: {
-      shadow: false, // Disable shadow for cleaner slides
-      slideShadows: false, // Disable shadows
-      shadowOffset: 20,
-      shadowScale: 0.94,
+    effect:'cube',
+    grabCursor:true,
+    cubeEffect:{
+      shadow:false,
+      slideShadows:false,
+      shadowOffset:0,
+      shadowScale:0.8,
+      perspective:1000,
+      modifier:1,
     },
-    loop: true,
-    autoplay: {
-      delay: 4000,
-      disableOnInteraction: false,
+    loop:true,
+    autoplay:{
+      delay:4000,
+      disableOnInteraction:false,
     },
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
+    pagination:{
+      el:'.swiper-pagination',
+      clickable:true,
     },
-    navigation: {
-      prevEl: '.swiper-button-prev',
-      nextEl: '.swiper-button-next',
+    navigation:{
+      prevEl:'.swiper-button-prev',
+      nextEl:'.swiper-button-next',
     },
   });
 });
 
+// const swiper3 = new Swiper('.swiper3', {
+//   speed:1000,
+//   loop:true,
+//   grabCursor:true,
+//   effect:"creative",
+//   creativeEffect:{
+//     prev:{
+//       shadow:true,
+//       translate:["-120%", 0, -500],
+//     },
+//     next:{
+//       shadow:true,
+//       translate:["120%", 0, -500],
+//     },
+//   },
+//   autoplay:{
+//     delay:4000,
+//     reverseDirection:false,
+//   },
+// });
